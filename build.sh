4a3ceb291dedf1c3d606ad6b0b1b3bd9cd9d4118
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-27 19:04:10 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 git pull
 as -c kernel/boot.S -o boot.o
 gcc -fpic -ffreestanding -std=gnu99 -c kernel/kernel.c -o kernel.o -O2 -Wall -Wextra
+-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+objcopy myos.elf -O binary kernel7.img
+qemu-system-arm -m 256 -M raspi2 -serial stdio -kernel kernel.elf
